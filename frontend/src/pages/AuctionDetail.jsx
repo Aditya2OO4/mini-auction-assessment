4828b85d79544f2c9a96c64c0c1511a0268b328f
Aditya2OO4 <125636723+Aditya2OO4@users.noreply.github.com>
2026-03-15 22:02:02 +0530

    Update auction API endpoints to production URLs

--- frontend/src/pages/AuctionDetail.jsx
+++ frontend/src/pages/AuctionDetail.jsx
@@ -9,7 +9,7 @@ function AuctionDetail({ user }) {
 
   const fetchAuction = () => {
 
-    fetch(`http://localhost:5000/auction/${id}`)
+    fetch(`http://mini-auction-assessment.onrender.com/auction/${id}`)
       .then(res => res.json())
       .then(data => setAuction(data));
 
@@ -27,7 +27,7 @@ function AuctionDetail({ user }) {
 
   const placeBid = async () => {
 
-    const response = await fetch("http://localhost:5000/bid/place",{
+    const response = await fetch("http://mini-auction-assessment.onrender.com/bid/place",{
 
       method:"POST",
 
