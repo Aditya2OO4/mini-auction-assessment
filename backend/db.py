0904a204a23acb554602a0c41834d898f7ce90bc
Aditya2OO4 <125636723+Aditya2OO4@users.noreply.github.com>
2026-03-16 13:44:29 +0530

    Include port in MySQL connection settings
    Add port configuration for MySQL connection

--- backend/db.py
+++ backend/db.py
@@ -7,5 +7,6 @@ def get_connection():
         host=os.environ.get("MYSQLHOST"),
         user=os.environ.get("MYSQLUSER"),
         password=os.environ.get("MYSQLPASSWORD"),
-        database=os.environ.get("MYSQLDATABASE")
+        database=os.environ.get("MYSQLDATABASE"),
+        port=int(os.environ.get("MYSQLPORT", 3306))
     )
